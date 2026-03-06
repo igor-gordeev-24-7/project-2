325461cb1eadc95168fb0508b03e5475db5ebfb2
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-03-06 13:13:06 +0300

    Изменена логика

--- src/main/java/org/example/utils/HistoryManager.java
+++ src/main/java/org/example/utils/HistoryManager.java
@@ -2,38 +2,33 @@ package org.example.utils;
 
 import org.example.model.Task;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public enum HistoryManager {
     HISTORY;
-    private List<Task> historyOfTasksRequest = new ArrayList<>();
+    private LinkedList<Task> historyList = new LinkedList<>();
+    private Map<Integer, Task> historyMap = new HashMap<>();
 
     public List<Task> getHistory() {
-        return historyOfTasksRequest;
+        return new ArrayList<>(historyList);
     }
 
-    public void getHistoryAsString() {
-        System.out.println("--------");
-        System.out.println("История поиска tasks: ");
-        for (Task task : historyOfTasksRequest) {
-            System.out.println("Id таска - " + task.getId());
-        }
-        System.out.println("--------");
-    }
-
-    public void deleteFromHistory(Task task) {
-        if (historyOfTasksRequest.contains(task)) {
-            historyOfTasksRequest.remove(task);
+    public void remove(Task task) {
+        if (historyMap.containsKey(task.getId())) {
+            historyMap.remove(task.getId());
+            historyList.remove(task);
         }
     }
 
-    public void addToHistory(Task task) {
-        historyOfTasksRequest.add(task);
-
-        if (historyOfTasksRequest.size() > 10) {
-            historyOfTasksRequest.remove(0);
+    public void add(Task task) {
+        if (task == null) {
+            return;
         }
+
+        remove(task);
+
+        historyMap.put(task.getId(), task);
+        historyList.addLast(task);
     }
 
 }
