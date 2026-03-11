35c65480ab8dac00b04d354fbf3a137d1c7325e5
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-03-12 00:07:07 +0300

    Добавлены методы удаления и добаления

--- src/main/java/org/example/service/InMemoryHistoryManager.java
+++ src/main/java/org/example/service/InMemoryHistoryManager.java
@@ -1,4 +1,18 @@
 package org.example.service;
 
+import org.example.model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InMemoryHistoryManager {
+    private final List<Task> history = new ArrayList<>();
+
+    public void addToHistory(Task task) {
+        history.add(task);
+    }
+
+    public void deleteFromHistory(Task task) {
+        history.remove(task);
+    }
 }
