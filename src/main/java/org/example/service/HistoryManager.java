7996e14c7dac89e819c2386a881202f8d42c0cc9
igor-gordeev-24-7 <igor.gordeev.24.7@gmail.com>
2026-03-17 14:45:09 +0300

    Закомментировал код

--- src/main/java/org/example/service/HistoryManager.java
+++ src/main/java/org/example/service/HistoryManager.java
@@ -5,8 +5,7 @@ import org.example.model.Task;
 import java.util.List;
 
 public interface HistoryManager {
-    void addToHistory(Task task);
-    void deleteFromHistory(Task task);
-    List<Task> getHistory();
-    String getHistoryAsString();
+//    void deleteFromHistory(Task task);
+//    List<Task> getHistory();
+//    String getHistoryAsString();
 }
